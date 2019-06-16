9bb1a91d147a2bc67f3e0ee20488033442f48af9
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-06-16 10:18:30 -0400

    added images

--- rm-front/src/components/CharacterCard.js
+++ rm-front/src/components/CharacterCard.js
@@ -5,9 +5,14 @@ const CharacterCard = (props) => {
   console.log(props)
   return(
     <div color='blue'>
+    <div>
+    <img src={props.character.image} />
+    <div>
     Character Name: {props.character.name}
     <div>
     Character Status: {props.character.status}
+    </div>
+    </div>
     </div>
     </div>
   )
