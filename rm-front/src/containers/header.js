41d0664874c859684078096e16557a981bdc6a87
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-20 11:54:56 -0400

    added header

--- rm-front/src/containers/header.js
+++ rm-front/src/containers/header.js
@@ -1,13 +1,15 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {Link} from 'react-router-dom'
 
-class Header extends Component {
-  render(){
-    return(
-      <header className='header'>
-        <p>This is the header.</p>
-      </header>
-    )
-  }
+
+const Header = (props) => {
+  return(
+    <div className="ui inverted olive menu navbar">
+     <Link to="/" className="item"><b>Home</b></Link>
+     <Link to="/favorites" className="item"><b>Favorites</b>
+     </Link>
+   </div>
+  )
 }
 
 export default Header
